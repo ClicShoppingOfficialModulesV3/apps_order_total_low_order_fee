3c0ec98830bac8766d597821f7bd683b580641b8
ClicShopping - e-commerce Solution <39338579+ClicShopping@users.noreply.github.com>
2018-11-06 17:36:38 -0500

    remove index.php from link

--- includes/ClicShopping/Apps/OrderTotal/LowOrderFee/Sites/ClicShoppingAdmin/Pages/Home/templates/main.php
+++ includes/ClicShopping/Apps/OrderTotal/LowOrderFee/Sites/ClicShoppingAdmin/Pages/Home/templates/main.php
@@ -49,7 +49,7 @@ use ClicShopping\OM\HTML;
             <div class="form-group">
               <div class="col-md-12">
 <?php
-  echo HTML::form('configure', CLICSHOPPING::link('CLICSHOPPING::link(null, 'A&OrderTotal\LowOrderFee&Configure'));
+  echo HTML::form('configure', CLICSHOPPING::link(null, 'A&OrderTotal\LowOrderFee&Configure'));
   echo HTML::button($CLICSHOPPING_LowOrderFee->getDef('button_configure'), null, null, 'primary');
   echo '</form>';
 ?>
