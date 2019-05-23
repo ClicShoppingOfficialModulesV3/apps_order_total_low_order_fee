1256f1736586f242de031e45f7405616bb0fbc4d
ClicShopping - e-commerce Solution <39338579+ClicShopping@users.noreply.github.com>
2019-05-23 19:38:48 -0400

    Add funding

--- includes/ClicShopping/Apps/OrderTotal/LowOrderFee/Module/ClicShoppingAdmin/Config/LO/Params/order_under.php
+++ includes/ClicShopping/Apps/OrderTotal/LowOrderFee/Module/ClicShoppingAdmin/Config/LO/Params/order_under.php
@@ -1,24 +1,27 @@
 <?php
-/**
- *
- *  @copyright 2008 - https://www.clicshopping.org
- *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
- *  @Licence GPL 2 & MIT
- *  @licence MIT - Portion of osCommerce 2.4
- *  @Info : https://www.clicshopping.org/forum/trademark/
- *
- */
+  /**
+   *
+   * @copyright 2008 - https://www.clicshopping.org
+   * @Brand : ClicShopping(Tm) at Inpi all right Reserved
+   * @Licence GPL 2 & MIT
+   * @licence MIT - Portion of osCommerce 2.4
+   * @Info : https://www.clicshopping.org/forum/trademark/
+   *
+   */
 
   namespace ClicShopping\Apps\OrderTotal\LowOrderFee\Module\ClicShoppingAdmin\Config\LO\Params;
 
-  class order_under extends \ClicShopping\Apps\OrderTotal\LowOrderFee\Module\ClicShoppingAdmin\Config\ConfigParamAbstract {
+  class order_under extends \ClicShopping\Apps\OrderTotal\LowOrderFee\Module\ClicShoppingAdmin\Config\ConfigParamAbstract
+  {
 
     public $sort_order = 30;
     public $default = '50';
     public $app_configured = true;
+
 //currencies->format
-    protected function init() {
-        $this->title = $this->app->getDef('cfg_order_total_low_order_fee_order_under_title');
-        $this->description = $this->app->getDef('cfg_order_total_low_order_fee_order_under_description');
+    protected function init()
+    {
+      $this->title = $this->app->getDef('cfg_order_total_low_order_fee_order_under_title');
+      $this->description = $this->app->getDef('cfg_order_total_low_order_fee_order_under_description');
     }
   }
